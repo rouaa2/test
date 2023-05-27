64f5c1e6108ea6db8a79345d71927c7c2c5eace6
rou教學用 <77996969+rouaa2@users.noreply.github.com>
2023-05-27 11:53:42 +0800

    Add files via upload

--- 111/dist/script.js
+++ 111/dist/script.js
@@ -1,1 +1,7 @@
-document.write("HELLO ---------這是JS語法")
+var count = 0;
+    var counterElement = document.getElementById("counter");
+
+    function increment() {
+      count++;
+      counterElement.textContent = count;
+    }
